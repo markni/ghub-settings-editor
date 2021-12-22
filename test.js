8b84831b8df4e75ecb7442c9f649a7c63360bd09
markni <xixian.ni@gmail.com>
2021-12-22 11:36:33 -0800

    remove unused file

--- test.js
+++
@@ -1,3 +0,0 @@
-const ghub = require('./index');
-
-ghub.add({applicationPath:'C:\\\\WindowsApps\\\\Microsoft.254428597CFE2_1.3266.27842.0_x64__8wekyb3d8bbwe\\\\HaloInfinite.exe', name:'', posterPath: ''});
